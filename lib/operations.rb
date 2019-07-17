93ed1bd38d7304bf0c405d0434447e1c582bf137
Natalie Taktachev <ntaktach@gmail.com>
2019-07-17 18:03:42 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,13 @@
 def unsafe?(speed)
-if speed < 40 || > 60 
-	return true 
-else 
-	return false 
+if speed < 40 || > 60
+	return true
+else
+	return false
 end
 end
 
 
 
 def not_safe?(speed)
-	
+
 end
-	
-
-
